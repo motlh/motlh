d13a66ad4ae0a90db7815adb0c5a4212610cb243
Andreyan Andreev <andreyan.andreev@live.com>
2017-04-25 17:41:09 +0300

    init

---
+++ src/bootstrap/app.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var motlh_framework_1 = require("motlh-framework");
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(<motlh_framework_1.App />, document.getElementById('root'));
---
+++ src/bootstrap/app.tsx
@@ -0,0 +1,5 @@
+import { App } from 'motlh-framework';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
